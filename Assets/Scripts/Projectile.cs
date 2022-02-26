e22f2fbd167e8f46b6797a71324d85a81a048a95
bhayden51 <bradley8451@hotmail.com>
2022-02-26 00:40:26 -0600

    fixed player prefab

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -20,36 +20,43 @@ public class Projectile : MonoBehaviour
         DestroyBullet(bulletDeathWait);
         transform.Rotate(0, 0, Random.Range(-bloom, bloom));
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject == myOwner)
-        {
-            DestroyBullet(0);
-        }
-        else if(collision.gameObject.layer == 10)//layer 10 is Walls
-        { 
 
-            if (bulletSpeed > 0)
-            {
-                bulletSpeed = 0;
-            }
-
-        }
-    }
     // Update is called once per frame
     void Update()
     {
         if (bulletSpeed < 0)
         {
-            transform.up = transform.position - myOwner.transform.position;
+            transform.up = (Vector2)transform.position - (Vector2)myOwner.transform.position;
         }
         transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
         bulletSpeed -= bulletSpeedDampening * Time.deltaTime;
+
+        if (bulletSpeed < 0 && Vector2.Distance(transform.position, myOwner.transform.position) < .2f)
+        {
+            DestroyBullet(0);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10)//layer 10 is Walls
+        {
+            if(bulletSpeedDampening == 0)
+            {
+                DestroyBullet(0);
+            }
+            else if (bulletSpeed > 0)
+            {
+                bulletSpeed = 0;
+            }
+        }
+    }
+
     IEnumerator LaserDamage(float seconds)
     {
         yield return new WaitForSeconds(seconds);
     }
+
     private void DestroyBullet(float seconds)
     {
         if(objectSpawnOnDeath != null)
@@ -61,7 +68,15 @@ public class Projectile : MonoBehaviour
 
     public void HitCharacter()
     {
-
+        if (bulletSpeedDampening == 0)
+            DestroyBullet(0);
+        else
+        {
+            if (bulletSpeed > 0)
+            {
+                bulletSpeed = 0;
+            }
+        }
     }
 
 }
