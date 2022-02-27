b855acf31107affa47ffc12d111794959d929608
codydoerr <40671276+codydoerr@users.noreply.github.com>
2022-02-26 22:49:20 -0600

    sent

--- Assets/Prefabs/Enemies/BossEnemies/TempMoveEnemyScript.cs
+++ Assets/Prefabs/Enemies/BossEnemies/TempMoveEnemyScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TempMoveEnemyScript : MonoBehaviour
 {
+    [SerializeField] GameObject moveHere;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class TempMoveEnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Rigidbody2D>().MovePosition(moveHere.transform.position);
     }
 }
