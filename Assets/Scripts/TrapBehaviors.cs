45742d80cbeb5e11a51d9be94b213b31f49f9ba5
codydoerr <40671276+codydoerr@users.noreply.github.com>
2022-02-26 22:46:06 -0600

    start on boss room and enemies

--- Assets/Scripts/TrapBehaviors.cs
+++ Assets/Scripts/TrapBehaviors.cs
@@ -6,10 +6,17 @@ public class TrapBehaviors : MonoBehaviour
 {
     [SerializeField] float time = 10;
     [SerializeField] GameObject spawn;
+    [SerializeField] GameObject[] enemies;
+    [SerializeField] GameObject curEnemies;
+    [SerializeField] GameObject boss;
     public bool roomNotEmpty;
     // Start is called before the first frame update
     void Start()
     {
+        if(enemies.Length !=0 && curEnemies)
+        {
+            curEnemies = enemies[0];
+        }
         roomNotEmpty = true;
         if (time != 0)
         {
@@ -20,14 +27,36 @@ public class TrapBehaviors : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemies.Length != 0)
+        {
+            if (boss)
+            {
+                if (boss.GetComponent<EnemyHealth>().currentSheild > 5)
+                {
+                    curEnemies = enemies[1];
+                }
+                else if (boss.GetComponent<EnemyHealth>().currentSheild > 10)
+                {
+                    curEnemies = enemies[2];
+                }
+                else if (boss.GetComponent<EnemyHealth>().currentSheild > 15)
+                {
+                    curEnemies = enemies[3];
+                }
+            }
+        }
     }
     IEnumerator laserTime(float seconds)
     {
         while (roomNotEmpty)
         {
             yield return new WaitForSeconds(seconds);
-            Instantiate(spawn,transform.position,transform.rotation);
+            if (enemies.Length != 0)
+            {
+                Instantiate(curEnemies, transform.position, transform.rotation);
+            }
+            else
+                Instantiate(spawn,transform.position,transform.rotation);
         }
     }
 }
